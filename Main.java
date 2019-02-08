d4522b450d3c77355712a007aa292c782715c03a
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-08 23:56:48 +0800

    Added functional loop

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args){
+    int i=5;
     do{
     System.out.println("Hello Word");
-    }while(true);
+    i--;
+  } while(i!=1);
 
 
   }
