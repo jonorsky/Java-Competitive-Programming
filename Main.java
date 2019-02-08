8fefd8ca1d01b8f520ce233976d0d65b7f4ec5ac
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-08 23:55:20 +0800

    Fix Errors

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
-public class Hello{
+public class Main{
   public static void main(String[] args){
     do{
     System.out.println("Hello Word");
-    while(true);
+    }while(true);
+
+
   }
 }
