1ddc95eb1015de9e2dc374b7991d064b44e80e40
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-11 23:11:31 +0800

    Add sample data

--- HashMap/Main2.java
+++ HashMap/Main2.java
@@ -4,5 +4,6 @@ import java.util.Scanner;
 public class Main2{
   public static void main(String[] args){
     HashMap <String,Integer> hm = new HashMap<String, Integer>();
+    hm.put("Philippines",1);
   }
 }
