1a3a4cc7af3ea1151b23d8fd6d7495461971bc5c
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-11 23:12:13 +0800

    Output certain value

--- HashMap/Main2.java
+++ HashMap/Main2.java
@@ -5,5 +5,7 @@ public class Main2{
   public static void main(String[] args){
     HashMap <String,Integer> hm = new HashMap<String, Integer>();
     hm.put("Philippines",1);
+
+    System.out.println(hm.get("Philippines"));
   }
 }
