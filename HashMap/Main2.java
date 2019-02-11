0fd0b3f65df143860b61fbe6acf14f380837e243
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-11 23:13:18 +0800

    Output Asia

--- HashMap/Main2.java
+++ HashMap/Main2.java
@@ -5,7 +5,9 @@ public class Main2{
   public static void main(String[] args){
     HashMap <String,Integer> hm = new HashMap<String, Integer>();
     hm.put("Philippines",1);
+    hm.put("Asia",1);
 
     System.out.println(hm.get("Philippines"));
+    System.out.println(hm.get("Asia"));
   }
 }
