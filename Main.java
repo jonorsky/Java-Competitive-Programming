aa487a5782c1ea859e01932b08f75a09515cba54
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-19 02:23:55 +0800

    Added search and position

--- Main.java
+++ Main.java
@@ -16,11 +16,21 @@ class Main{
     stack.pop();
   }
 
+  static void stack_search(Stack<Integer> stack, int element){
+    int pos = stack.search(element);
+
+    if(pos == -1)
+      System.out.println("Element not found!");
+    else
+      System.out.println("Element found at position: " + pos);
+  }
+
   public static void main(String[] args){
     Stack<Integer> arr = new Stack<Integer>();
     stack_push(arr);
     print_top(arr);
     delete_top(arr);
     print_top(arr);
+    stack_search(arr, 3);
   }
 }
