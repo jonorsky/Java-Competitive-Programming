4cb94456c042bba0ae54ad02783a8782bd2e31a3
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-19 02:42:57 +0800

    Runtime Polymorphism

--- Main.java
+++ Main.java
@@ -1,36 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 class Main{
-  static void stack_push(Stack<Integer> stack){
-    for(int i=0; i<5; i++){
-      stack.push(i);
-    }
+  static void poly(int x){
+    System.out.println(x + "Its Integer!");
   }
 
-  static void print_top(Stack<Integer> stack){
-      System.out.println(stack.peek());
+  static void poly(double x){
+    System.out.println( x + "Its Double!");
   }
 
-  static void delete_top(Stack<Integer> stack){
-    stack.pop();
+  static void poly(char x){
+    System.out.println(x + "Its character!");
   }
 
-  static void stack_search(Stack<Integer> stack, int element){
-    int pos = stack.search(element);
-
-    if(pos == -1)
-      System.out.println("Element not found!");
-    else
-      System.out.println("Element found at position: " + pos);
+  static void poly(String x){
+    System.out.println("Its String!");
   }
 
   public static void main(String[] args){
-    Stack<Integer> arr = new Stack<Integer>();
-    stack_push(arr);
-    print_top(arr);
-    delete_top(arr);
-    print_top(arr);
-    stack_search(arr, 3);
+    Scanner in = new Scanner(System.in);
+    int x = 123;
+    double y = 2.32;
+    char z = 'A';
+
+      poly(x);
+      poly(y);
+      poly(z);
+
   }
 }
