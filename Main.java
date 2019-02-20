c5cf7953e46cec6c71040feb75a7a1b2871d91f6
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-20 21:41:24 +0800

    Byte Content

--- Main.java
+++ Main.java
@@ -9,5 +9,13 @@ public class Main{
     for(int i: arr){
       System.out.println(i);
     }
+    byte x[] = new byte[4];
+    for(byte i=0; i<4; i++){
+      x[i] = i;
+    }
+    System.out.println();
+    for(byte temp: x){
+      System.out.println(temp);
+    }
   }
 }
