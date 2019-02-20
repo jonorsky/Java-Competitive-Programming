b987d37668996a62ca00f064a83f278e9d38b205
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-20 21:37:49 +0800

    Array Test

--- Main.java
+++ Main.java
@@ -1,31 +1,13 @@
 import java.util.Scanner;
 
-class Main{
-  static void poly(int x){
-    System.out.println(x + "Its Integer!");
-  }
-
-  static void poly(double x){
-    System.out.println( x + "Its Double!");
-  }
-
-  static void poly(char x){
-    System.out.println(x + "Its character!");
-  }
-
-  static void poly(String x){
-    System.out.println("Its String!");
-  }
-
+public class Main{
   public static void main(String[] args){
-    Scanner in = new Scanner(System.in);
-    int x = 123;
-    double y = 2.32;
-    char z = 'A';
-
-      poly(x);
-      poly(y);
-      poly(z);
-
+    int arr[] = new int[20];
+    for(int i=0; i<5; i++){
+      arr[i] = i;
+    }
+    for(int i: arr){
+      System.out.println(i);
+    }
   }
 }
