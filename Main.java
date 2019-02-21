47857fc0f5720e0934549ff33af84e33ea69de64
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-21 10:47:17 +0800

    Using StringBuilder in Override

--- Main.java
+++ Main.java
@@ -8,5 +8,39 @@ class Person{
     this.name = name;
   }
 
-  public void 
+  public void setCourse(String course){
+    this.course = course;
+  }
+
+  public void setId(int id){
+    this.id = id;
+  }
+
+  @Override
+  public String toString(){
+    StringBuilder str = new StringBuilder("");
+
+    str.append("Name: " + name + "\n");
+    str.append("ID Number: " + id + "\n");
+    str.append("School: " + SCHOOL + "\n");
+    str.append("Course: " + course);
+
+    return str.toString();
+  }
+}
+
+public class Main{
+  public static void main(String[] args){
+
+    String str = "edabun";
+
+    str[1] = "x";
+
+    Person obj = new Person();
+    obj.setName("Edabun");
+    obj.setId(201523231);
+    obj.setCourse("Bachelor of Science in Information Technology");
+
+    System.out.println(obj);
+  }
 }
