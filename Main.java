a201552362d9a0ad2e93968cb9014d474783a799
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-21 10:22:18 +0800

    Multithreading

--- Main.java
+++ Main.java
@@ -1,35 +1,24 @@
-import java.util.Scanner;
+/**
+ * Multithreading in Java
+ */
+
+class MultithreadingDemo extends Thread{
+  public void run(){
+    try{
+      System.out.println("Thread " + Thread.currentThread().getId() + " is running");
+    }
+    catch(Exception e) {
+      System.out.println("Exception is caught");
+    }
+  }
+}
 
 public class Main{
-  public static void main(String[] args){
-    int arr[] = new int[20];
-    for(int i=0; i<5; i++){
-      arr[i] = i;
+    public static void main(String[] args){
+      int n = 8; // Number of Threads
+      for(int i=0; i<8; i++){
+        MultithreadingDemo object = new MultithreadingDemo();
+        object.start();
+      }
     }
-    for(int i: arr){
-      System.out.println(i);
-    }
-    byte x[] = new byte[4];
-    for(byte i=0; i<4; i++){
-      x[i] = i;
-    }
-    System.out.println();
-    for(byte temp: x){
-      System.out.println(temp);
-    }
-    String str = "test";
-    for(int i=0; i<str.length(); i++){
-      System.out.println(str.charAt(i));
-    }
-    StringBuilder sb = new StringBuilder("Hello World");
-    sb.append(" Java");
-    System.out.println(sb);
-    sb.insert(1,"xx");
-    System.out.println(sb);
-    sb.replace(1,3,"");
-    System.out.println(sb);
-    String str1 = "GRF";
-    StringBuilder strb = new StringBuilder(str1);
-    System.out.println(strb);
-  }
 }
