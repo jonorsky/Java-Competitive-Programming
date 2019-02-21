e57022bba7df0af3fe988f6bd6bcf2d65c05c7c3
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-21 13:09:26 +0800

    Applying Super Keyword

--- Main.java
+++ Main.java
@@ -1,72 +1,18 @@
-import java.util.Scanner;
-
 class Person{
-  private String name;
-  private static final String SCHOOL = "Ateneo de Naga University";
-  private String course;
-  private int id;
-
-  public Person(){} // Runtime Polymorph
-
-  public Person(String name, String couse, int id){
-    this.name = name;
-    this.course = couse;
-    this.id = id;
-  }
-
-  public void setName(String name){
-    this.name = name;
-  }
-
-  public void setCourse(String course){
-    this.course = course;
-  }
-
-  public void setId(int id){
-    this.id = id;
-  }
-
-  @Override
-  public String toString(){
-    StringBuilder str = new StringBuilder();
-
-    str.append("Name: " + name + "\n");
-    str.append("ID Number: " + id + "\n");
-    str.append("School: " + SCHOOL + "\n");
-    str.append("Course: " + course);
-
-    return str.toString();
+  public String name = "Edabun";
+  void psg(){
+    System.out.println("Hello from main class");
   }
 }
 
-public class Main{
+public class Main extends Person{
+  public void duh(){
+    super.psg();
+    System.out.println("Hello from subclass");
+
+  }
   public static void main(String[] args){
-    Scanner in = new Scanner(System.in);
-    Person obj = new Person();
-    obj.setName("Edabun");
-    obj.setId(201523231);
-    obj.setCourse("Bachelor of Science in Information Technology");
-
-
-    Person arr[] = new Person[2];
-    System.out.println("Input 2 Students with their Name,ID, and Course. \n");
-
-    for(int i=0; i<2; i++){
-      System.out.println("Student # " + (i+1));
-      arr[i] = new Person();
-      System.out.print("Input Name: ");
-      arr[i].setName(in.next());
-      System.out.print("Input ID: ");
-      arr[i].setId(in.nextInt());
-      in.nextLine();
-      System.out.print("Input Course: ");
-      arr[i].setCourse(in.next());
-      System.out.println();
-    }
-
-    for(Person i:arr){
-      System.out.println(i + "\n");
-    }
-
+    Main n = new Main();
+    n.duh();
   }
 }
