a51604f3b9f8ff4a251c403db9f58ceb488b0afb
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-22 14:19:52 +0800

    Passing Array in Method

--- Main.java
+++ Main.java
@@ -1,13 +1,26 @@
 import java.util.Arrays;
 
 public class Main{
-  public static void main(String[] args){
-    StringBuilder str[] = new StringBuilder[5];
-    for(int i=0; i<str.length; i++){
-      str[i] = new StringBuilder("" + i);
+  static void assignArray(int arr[]){
+    arr[0] = 999;
+    arr[1] = 888;
+  }
+  static void displayArray(int arr[]){
+    for(int x : arr){
+      System.out.println(x);
     }
-    for(StringBuilder temp : str){
-      System.out.println(temp);
+  }
+
+  public static void main(String[] args){
+    int arr[] = new int[2];
+    arr[0] = 1;
+    arr[1] = 2;
+
+    assignArray(arr);
+    displayArray(arr);
+
+    for(int x : arr){
+      System.out.println(x);
     }
   }
 }
