98a6d3b51c6ae18ce4d0d1fc935369c71f1b0abb
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-22 17:10:13 +0800

    Array Fill

--- Main.java
+++ Main.java
@@ -1,26 +1,12 @@
 import java.util.Arrays;
 
 public class Main{
-  static void assignArray(int arr[]){
-    arr[0] = 999;
-    arr[1] = 888;
-  }
-  static void displayArray(int arr[]){
-    for(int x : arr){
-      System.out.println(x);
-    }
-  }
-
   public static void main(String[] args){
-    int arr[] = new int[2];
-    arr[0] = 1;
-    arr[1] = 2;
-
-    assignArray(arr);
-    displayArray(arr);
-
+    int arr[] = new int[5];
+    Arrays.fill(arr,232);
     for(int x : arr){
       System.out.println(x);
     }
+
   }
 }
