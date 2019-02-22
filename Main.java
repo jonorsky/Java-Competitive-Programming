74e4174a16ec81ebc8e69522acda312ba6d4ba6d
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-22 13:18:43 +0800

    Declared constant

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
-public class Main{
-  public static void main(String[] args){
-    int ages[] = {35, 10, 43, -5, 12};
-    
 
+
+public class Main{
+  static final int SIZE = 5;
+
+  public static void main(String[] args){
+    int ages[] = new int[SIZE];
+    for(int i=0; i<ages.length; i++){
+      System.out.println(ages[i]);
+    }
   }
 }
