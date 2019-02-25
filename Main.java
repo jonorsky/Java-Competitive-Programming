059470dccbe12d6bd3ef2918e6206cd4cd3c05ca
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-25 23:26:17 +0800

    Initialize Main

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args){
-    int arr[] = new int[5];
-    Arrays.fill(arr,232);
-    for(int x : arr){
-      System.out.println(x);
-    }
-
+    
   }
 }
