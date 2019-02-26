d9c529a51168ec45326f55c188b2d021c5ba70d9
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-26 22:29:10 +0800

    Arraylist Add

--- Main.java
+++ Main.java
@@ -2,9 +2,8 @@ import java.util.ArrayList;
 
 public class Main{
   public static void main(String[] args){
-    ArrayList list1 = new ArrayList();
-    ArrayList list2 = new ArrayList(5);
-
-    ArrayList<String> list3 = new ArrayList<String>();
+    ArrayList<String> creatures = new ArrayList<String>();
+    creatures.add("Mutant");
+    System.out.println(creatures);
   }
 }
