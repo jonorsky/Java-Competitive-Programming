74d685afa156aa896bfc7a7c499b9fb0ff890541
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-26 22:30:21 +0800

    Insert in particular index

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main{
   public static void main(String[] args){
     ArrayList<String> creatures = new ArrayList<String>();
     creatures.add("Mutant");
+    creatures.add("Hello World");
+    creatures.add(1,"Human");
     System.out.println(creatures);
+
   }
 }
