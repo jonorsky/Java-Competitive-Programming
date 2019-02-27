52943df5c21568faf316660ef0d1c04a2603614c
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-27 23:39:05 +0800

    Implementing Encapsulation

--- Main.java
+++ Main.java
@@ -1,18 +1,11 @@
 import java.util.ArrayList;
 
+class Library{
+  
+}
+
 public class Main{
   public static void main(String[] args){
-    ArrayList<String> creatures = new ArrayList<String>();
-    creatures.add("Mutant");
-    creatures.add("Hello World");
-    creatures.add(1,"Human");
-    System.out.println(creatures);
 
-    ArrayList<String> cuddles = new ArrayList<String>();
-    cuddles.add("1");
-    cuddles.add("2");
-
-    creatures.addAll(2,cuddles);
-    System.out.println(creatures);
   }
 }
