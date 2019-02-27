def2c6e365488a6f0881a6c7d42e98c89b4e7617
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-27 23:41:30 +0800

    Initialize Book Class

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 import java.util.ArrayList;
 
+class Book{
+  public String title;
+}
+
 class Library{
-  
+  private ArrayList<Book> books = new ArrayList<Book>();
+
+  public Book getBook(int index){
+    return books.get(index);
+  }
 }
 
 public class Main{
