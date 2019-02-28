b4c3e0fb6e3b5676f8b8c2e7f448af7d01fdecda
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-28 23:07:34 +0800

    Changed to ArrayList

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
   public static void main(String[] args){
-    int arr[][] = new int[2][3];
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("dog");
+    
   }
 }
