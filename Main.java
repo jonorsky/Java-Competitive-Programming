eab2290aad9fab6866b1fd7e1d61c5b69b51e677
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-28 23:10:18 +0800

    Function for indexOf and contains

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main{
   public static void main(String[] args){
     ArrayList<String> list = new ArrayList<String>();
     list.add("dog");
+    list.add("cat");
 
+    System.out.println(list.indexOf("cat"));
+    System.out.println(list.contains("cat"));
   }
 }
