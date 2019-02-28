e2bef50704cca311961750fc8411399617726634
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-02-28 23:05:44 +0800

    Declare 2D Array

--- Main.java
+++ Main.java
@@ -1,31 +1,5 @@
-import java.util.ArrayList;
-
-class Book{
-  public String title;
-}
-
-class Library{
-  private ArrayList<Book> books = new ArrayList<Book>();
-
-  public Book getBook(int index){
-    return books.get(index);
-  }
-
-  public void addBook(Book book){
-    books.add(book);
-  }
-
-  public List getBooks(){
-    ArrayList list = new ArrayList(books.size());
-    for(Book book : books){
-      list.add(new Book(book));
-    }
-    return list;
-  }
-}
-
 public class Main{
   public static void main(String[] args){
-    Library lbadnu = new Library();
+    int arr[][] = new int[2][3];
   }
 }
