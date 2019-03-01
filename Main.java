d17c0c5b50b4d47bd3f08090b0f1258cc75a07be
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-01 22:33:50 +0800

    Break Statement

--- Main.java
+++ Main.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class Main{
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
-    int sum = 0;
-    int number;
-    do {
-      System.out.println("Enter a number: ");
-      number = in.nextInt();
-      if(number>0){
-        sum += number;
-      }
-    }while(number>0);
-    System.out.println("The sum is " + sum);
+    int n;
+    while(true){
+      n = in.nextInt();
+      if(n==0)
+        break;
+    }
   }
 }
