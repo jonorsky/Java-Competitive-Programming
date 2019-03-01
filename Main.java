980032e0519a74e0d02c8f6cdc7f9f9de1e764bf
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-01 22:35:55 +0800

    Nested Loop in for loop

--- Main.java
+++ Main.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args){
-    Scanner in = new Scanner(System.in);
-    int n;
-    while(true){
-      n = in.nextInt();
-      if(n==0)
-        break;
+    for(int i=0; i<10; i++){
+      for(int j=0; j<10; j++){
+        System.out. println(i + " " + j);
+      }
+      System.out.println();
     }
   }
 }
