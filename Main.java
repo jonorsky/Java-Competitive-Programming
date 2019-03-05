a2a5b4a9addeed4828ccc49afc15354fb404af82
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-05 22:41:30 +0800

    Tricky while statement

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
   public static void main(String[] args){
     int i = 5;
     int j = 10;
-    while(i<j){
+    while((i=12)!=5){
       System.out.println(i);
       i++;
     }
