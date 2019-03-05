52be9f90630f7653436c6d67b7b978a5f27fc8c2
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-05 22:42:51 +0800

    For each in array

--- Main.java
+++ Main.java
@@ -2,11 +2,9 @@ import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args){
-    int i = 5;
-    int j = 10;
-    while((i=12)!=5){
-      System.out.println(i);
-      i++;
+    int arr[] = {1,2,3,4,5};
+    for(int n : arr){
+      System.out.println(n);
     }
   }
 }
