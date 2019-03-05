f753f7ea41ac3fdde8fa8bb95e5e521fb94a16e2
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-05 22:59:41 +0800

    Loop using Iterator

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main{
   public static void main(String[] args){
@@ -12,6 +13,16 @@ public class Main{
     for(int n=0; n<list.size(); n++){
       System.out.println(list.get(n));
     }
-  
+    System.out.println();
+
+    for(String n : list){
+      System.out.println(n);
+    }
+
+    Iterator it = list.iterator();
+    while(it.hasNext()){
+      System.out.println(it.next());
+    }
+
   }
 }
