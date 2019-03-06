d2b9314b5d2f0b9f7aa40d3fbcbf919634a448ec
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-06 23:26:29 +0800

    Declare Employee Class

--- Main.java
+++ Main.java
@@ -1,28 +1,11 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Iterator;
+
+class Employee{
+  
+}
 
 public class Main{
   public static void main(String[] args){
-    List<String> list = new ArrayList<String>();
-    list.add("a");
-    list.add("b");
-    list.add("c");
-
-    for(int n=0; n<list.size(); n++){
-      System.out.println(list.get(n));
-    }
-    System.out.println();
-
-    for(String n : list){
-      System.out.println(n);
-    }
-
-    Iterator it = list.iterator();
-    while(it.hasNext()){
-      System.out.println(it.next());
-    }
 
   }
 }
