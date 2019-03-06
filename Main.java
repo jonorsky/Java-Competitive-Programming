49cbd81edd72a20a2f40052dc8e1f75420c25c34
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-06 23:34:53 +0800

    Changed function into public

--- Main.java
+++ Main.java
@@ -8,13 +8,14 @@ class Employee{
     return age;
   }
 
-  private void setAge(int age){
+  public void setAge(int age){
     this.age = age;
   }
 }
 
 public class Main{
   public static void main(String[] args){
-
+    Employee n = new Employee();
+    n.setAge(1);
   }
 }
