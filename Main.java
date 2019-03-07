c72598c07f3da1960ba6cf1b0e468d78e4e420ad
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-07 23:26:14 +0800

    New Main

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main extends Thread{
   public void run(){
-
+    System.out.println("My thread is in running state!");
   }
 
   public static void main(String[] args){
-
+    Main obj = new Main();
   }
 }
