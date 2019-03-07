8a4f2a3e9e3bb26807ca77d1832a088c5386036e
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-07 23:21:22 +0800

    Implementing Multithreading

--- Main.java
+++ Main.java
@@ -1,21 +1,11 @@
 import java.util.Scanner;
 
-class Employee{
-  private String name;
-  private int age;
-
-  public int getAge(){
-    return age;
-  }
-
-  public void setAge(int age){
-    this.age = age;
-  }
-}
-
 public class Main{
+  static int recursion(int n){
+
+  }
+
   public static void main(String[] args){
-    Employee n = new Employee();
-    n.setAge(1);
+
   }
 }
