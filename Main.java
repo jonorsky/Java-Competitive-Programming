a5d94c5242346c31299748d1520ae14fdbdb5365
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-12 20:39:39 +0800

    Big Integer Implementation

--- Main.java
+++ Main.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
 public class Main{
-  public static void print(){
-    
-  }
   public static void main(String[] args){
-    System.out.println();
+    Scanner in = new Scanner(System.in);
+    StringTokenizer token;
+    BigInteger bg;
+    String input,str1="",str2="";
+    while(true){
+      input = in.nextLine();
+      token = new StringTokenizer(input);
+      bg = new BigInteger("0");
+      boolean flag = true;
+      while(token.hasMoreTokens()){
+        String psg = token.nextToken().toString();
+        //System.out.println(token.nextToken().toString());
+        if(flag==true)
+          str1 = psg;
+        else
+          str2 = psg;
+      }
+      BigInteger val1 = new BigInteger("0");
+      BigInteger val2 = new BigInteger("0");
+      val1 = new BigInteger("232");
+      val2 = new BigInteger("232");
+      val1.add(val2);
+    }
+
   }
 }
