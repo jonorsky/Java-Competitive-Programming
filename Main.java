1e7b94ce315d451430705267a3069574b0dd1062
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-12 21:22:45 +0800

    Read from file

--- Main.java
+++ Main.java
@@ -1,31 +1,21 @@
 import java.util.Scanner;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Main{
   public static void main(String[] args){
-    Scanner in = new Scanner(System.in);
-    StringTokenizer token;
-    BigInteger bg;
-    String input,str1="",str2="";
-    while(true){
-      input = in.nextLine();
-      token = new StringTokenizer(input);
-      bg = new BigInteger("0");
-      boolean flag = true;
-      while(token.hasMoreTokens()){
-        String psg = token.nextToken().toString();
-        //System.out.println(token.nextToken().toString());
-        if(flag==true)
-          str1 = psg;
-        else
-          str2 = psg;
-      }
-      BigInteger val1 = new BigInteger("0");
-      BigInteger val2 = new BigInteger("0");
-      val1 = new BigInteger("232");
-      val2 = new BigInteger("232");
-      val1.add(val2);
+    Scanner in = new Scanner("C:\\Users\\Admin\\Desktop\\jav");
+    int x,y,z;
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    do{
+      x = in.nextInt();
+      y = in.nextInt();
+      z = in.nextInt();
+      list.add(y);
+      System.out.println(x + " " + y + " " + z);
+    } while(in.hasNextInt());
+
+    for(int xx : list){
+      System.out.println(xx);
     }
 
   }
