2aaf8ed12ff6cd78a1932d6688d0995f95c3903f
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-12 22:00:11 +0800

    BufferedReader for Java

--- Main.java
+++ Main.java
@@ -5,22 +5,26 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
 import java.util.StringTokenizer;
+import java.io.InputStreamReader;
 
 public class Main{
   public static void main(String[] args){
     File file = new File("C:\\Users\\Admin\\Desktop\\jav\\hey.txt");
 
     try{
-      BufferedReader br = new BufferedReader(new FileReader(file));
+      //BufferedReader br = new BufferedReader(new FileReader(file));
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
       String st;
       ArrayList<Integer> list = new ArrayList<Integer>();
-      while ((st = br.readLine()) != null){
+      while ((st=br.readLine()) != null){
         StringTokenizer st1 = new StringTokenizer(st, " ");
         while (st1.hasMoreTokens()){
-        //  list.add(Integer.parseInt(st1.nextToken()));
-          System.out.print(Integer.parseInt(st1.nextToken()));
+          list.add(Integer.parseInt(st1.nextToken()));
+          //System.out.print(Integer.parseInt(st1.nextToken()));
         }
-        //System.out.println(list.get(1));
+        System.out.println(list.get(1));
+        list.clear();
       }
     }catch(IOException e){
 
