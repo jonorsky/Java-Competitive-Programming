c931853699a7d898e43e928d19379312fcdaa004
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-12 21:38:30 +0800

    Read from file template

--- Main.java
+++ Main.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
 
 public class Main{
   public static void main(String[] args){
-    Scanner in = new Scanner("C:\\Users\\Admin\\Desktop\\jav");
-    int x,y,z;
-    ArrayList<Integer> list = new ArrayList<Integer>();
-    do{
-      x = in.nextInt();
-      y = in.nextInt();
-      z = in.nextInt();
-      list.add(y);
-      System.out.println(x + " " + y + " " + z);
-    } while(in.hasNextInt());
 
-    for(int xx : list){
-      System.out.println(xx);
+    File file = new File("C:\\Users\\Admin\\Desktop\\jav\\hey.txt");
+
+    try{
+    BufferedReader br = new BufferedReader(new FileReader(file));
+    String st;
+    while ((st = br.readLine()) != null)
+    
+    } catch(IOException e){
+
     }
+
+
 
   }
 }
