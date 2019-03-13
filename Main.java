c105f7171edfab438a849a7ce19d1b60ffad46e9
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-13 22:57:00 +0800

    Fix import Date library

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import java.util.Date;
+
 final class Header{
   private final String title;
   private final int version;
   private final Date date;
+
+  public Header(String tile, int version, Date date){
+    this.title = tile;
+    this.version = version;
+    this.date = new Date(date.getTime());
+  }
+
 }
 
 public class Main{
