4adee14536f338b681df97267c2677c5833e5b22
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-18 23:45:55 +0800

    Testing Exception

--- Main.java
+++ Main.java
@@ -1,14 +1,9 @@
-class Animal{
-
-}
-
-class Dog extends Animal{
-
-}
+import java.io.IOException;
 
 public class Main{
   public static void main(String[] args){
-    Dog d = new Dog();
-    System.out.println( d instanceof Animal);
+    try{
+      throw new Exception();
+    }catch(Exception e){}
   }
 }
