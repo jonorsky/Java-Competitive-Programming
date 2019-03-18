b9ccf657daeaa163ae3c307eb856cff7b730d11b
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-03-18 23:46:25 +0800

    Output exception

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main{
   public static void main(String[] args){
     try{
       throw new Exception();
-    }catch(Exception e){}
+    }catch(Exception e){
+      System.out.println(e);
+    }
   }
 }
